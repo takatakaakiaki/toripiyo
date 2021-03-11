84f614e61cdaa9fb05fce186ef9d744a211f3b85
takatakaakiaki <xiaozhangzuoteng903@gmail.com>
2021-03-11 15:27:51 +0900

    モデル、コントローラーの記述
    バリデーションの設定
    index new create

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,26 @@
 class TweetsController < ApplicationController
   
   def index
+    @tweets = Tweet.all.order('created_at DESC')
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  
+  prevate
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :article, :category_id, :prefecture_id, :city).merge(user_id: current_user.id)
+  end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,18 @@
 class Tweet < ApplicationRecord
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :article
+    validates :category_id
+    validates :prefecture_id
+    validates :city
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :prefecture_id
+  end
 end
