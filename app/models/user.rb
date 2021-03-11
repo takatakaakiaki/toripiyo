4cd3f4f76bdb59e8954e5405779f05248e4ebbe4
takatakaakiaki <xiaozhangzuoteng903@gmail.com>
2021-03-11 10:13:18 +0900

    微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,12 @@ class User < ApplicationRecord
   end
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
     validates :last_name
+    validates :first_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角カタカナで入力してください' } do
-    validates :first_name_kana
     validates :last_name_kana
+    validates :first_name_kana
   end
 end
