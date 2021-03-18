2eb44de95e672ea9cd9c987d692ea6ed1cac97c8
takatakaakiaki <xiaozhangzuoteng903@gmail.com>
2021-03-18 16:03:34 +0900

    ビューの調整

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,13 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: 'しぐさ' },
-    { id: 3, name: '遊び' },
+    { id: 2, name: '普段の様子' },
+    { id: 3, name: '飼育について' },
     { id: 4, name: '飼育用品' },
     { id: 5, name: 'おもちゃ' },
-    { id: 6, name: 'エサ' },
-    { id: 7, name: '睡眠' },
-    { id: 8, name: 'ショップ' },
-    { id: 9, name: '病院' },
-    { id: 10, name: 'その他' }
+    { id: 6, name: 'ショップ' },
+    { id: 7, name: '病院' },
+    { id: 8, name: 'その他' },
   ]
 
   include ActiveHash::Associations
