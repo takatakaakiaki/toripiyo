a25634cc5cd68513715c44d836b4be02f7ab3cd4
takatakaakiaki <xiaozhangzuoteng903@gmail.com>
2021-03-27 18:01:38 +0900

    結合テスト
    ツイート編集機能のテスト

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -6,7 +6,7 @@ def basic_pass
   visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
 end
 
-RSpec.describe "Tweets", type: :system do
+RSpec.describe "ツイート投稿", type: :system do
   before do
     @user = FactoryBot.create(:user)
     @tweet_title = Faker::Lorem.sentence
@@ -72,12 +72,124 @@ RSpec.describe "Tweets", type: :system do
       expect(current_path).to eq('/users/sign_in')
     end
   end
-
-  context '' do
-
-
-
-
-
-
 end
+
+RSpec.describe "ツイート編集", type: :system do
+  before do
+    @tweet1 = FactoryBot.create(:tweet)
+    @tweet2 = FactoryBot.create(:tweet)
+    sleep(1)
+  end
+
+  context '自分が投稿した記事の編集ができる' do
+    it '自分が投稿した記事ならば編集ができる' do
+      # Basic認証の入力をする
+      basic_pass
+
+      # ツイート2を投稿したユーザーがログインする
+      visit new_user_session_path
+      fill_in 'user[email]', with: @tweet2.user.email
+      fill_in 'user[password]', with: @tweet2.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+
+      # ツイート2をクリックする
+      page.first("img[src$='inko-sample.png']").click
+
+      # ツイート2の詳細ページに遷移したことを確認する 
+      expect(current_path).to eq(tweet_path(@tweet2.id))
+
+      # 編集ページへのリンクがあることを確認する
+      expect(page).to have_content('記事の編集')
+
+      # 記事の編集をクリックする
+      click_link '記事の編集'
+
+      # ツイート2の編集ページに遷移したことを確認する
+      expect(current_path).to eq(edit_tweet_path(@tweet2.id))
+
+      # 既に投稿済みの内容が入力されていることを確認する
+      expect(
+        find('#tweet-title').value
+      ).to eq(@tweet2.title)
+      expect(
+        find('#tweet-article').value
+      ).to eq(@tweet2.article)
+      expect(
+        find('#tweet-category').value
+      ).to eq"#{@tweet2.category_id}"
+      expect(
+        find('#tweet-prefecture').value
+      ).to eq"#{@tweet2.prefecture_id}"
+      expect(
+        find('#tweet-city').value
+      ).to eq(@tweet2.city)
+
+      # ツイートを編集する
+      attach_file 'tweet[image]', "app/assets/images/inko-logo.png"
+      fill_in 'tweet[title]', with: "#{@tweet2.title}+編集したタイトル"
+      fill_in 'tweet[article]', with: "#{@tweet2.article}+編集した記事"
+      select 'ショップ', from: 'tweet-category'
+      select '沖縄', from: 'tweet-prefecture'
+      fill_in 'tweet[city]', with: "#{@tweet2.city}+編集した市町村"
+
+      # 編集してもTweetモデルのカウントが変わらないことを確認する
+      expect {
+        find('input[name="commit"]').click
+      }.to change {Tweet.count}.by(0)
+
+      # ツイートの詳細ページに遷移したことを確認する
+      expect(current_path).to eq(tweet_path(@tweet2.id))
+
+      # ツイートの編集内容が反映されていることを確認する(画像)
+      expect(page).to have_selector"img[src$='inko-logo.png']"
+
+      # ツイートの編集内容が反映されていることを確認する(テキスト)
+      expect(page).to have_content(@tweet2_title)
+
+      # トップページへ遷移する
+      visit root_path
+
+      # 編集したツイートがトップページにあることを確認する(画像) 
+      expect(page).to have_selector"img[src$='inko-logo.png']"
+
+      # 編集したツイートがトップページにあることを確認する(テキスト)
+      expect(page).to have_content(@tweet2_title)
+    end
+  end
+
+  context '他の人が投稿した記事は編集できない' do
+    it '自分が投稿した記事ではないならば編集はできない' do
+      # ツイート1を投稿したユーザーがログインする
+      visit new_user_session_path
+      fill_in 'user[email]', with: @tweet1.user.email
+      fill_in 'user[password]', with: @tweet1.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+
+      # ツイート2をクリックする
+      page.first("img[src$='inko-sample.png']").click      
+
+      # ツイート2の詳細ページに遷移したことを確認する 
+      expect(current_path).to eq(tweet_path(@tweet2.id))
+
+      # 編集ページへのリンクがないことを確認する
+      expect(page).to have_no_content('記事の編集')
+    end
+
+    it 'ログインしていないと記事の編集はできない' do
+      # トップページに遷移する
+      visit root_path
+
+      # ツイート2をクリックする
+      page.first("img[src$='inko-sample.png']").click            
+      
+      # ツイート2の詳細ページに遷移したことを確認する 
+      expect(current_path).to eq(tweet_path(@tweet2.id))
+
+      # 編集ページへのリンクがないことを確認する
+      expect(page).to have_no_content('記事の編集')
+    end
+  end
+end
+
